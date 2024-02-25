6e32c91442392b10bb148228041974a12f25ece1
razermanth <razermanth@gmail.com>
2024-02-25 15:36:09 +0700

    [FIX] unuse file

--- frontend/src/app/WarningMessage.tsx
+++
@@ -1,8 +0,0 @@
-// WarningMessage.tsx
-import React from 'react';
-
-const WarningMessage: React.FC = () => {
-  return <p>This website is only viewable in landscape mode</p>;
-};
-
-export default WarningMessage;
