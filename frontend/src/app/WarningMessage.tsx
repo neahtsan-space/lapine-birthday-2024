f6d7676708f8f0b974929ea9ad9270594e6f5451
razermanth <razermanth@gmail.com>
2024-02-25 15:31:55 +0700

    [FIX] tablet responsive on portrait

---
+++ frontend/src/app/WarningMessage.tsx
@@ -0,0 +1,8 @@
+// WarningMessage.tsx
+import React from 'react';
+
+const WarningMessage: React.FC = () => {
+  return <p>This website is only viewable in landscape mode</p>;
+};
+
+export default WarningMessage;
